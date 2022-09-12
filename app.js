f2bf2d6b5d2b0cf52dfd0215db4293b384524a5b
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2022-09-13 01:46:04 +0600

    iterator use to next and prev data

--- app.js
+++ app.js
@@ -5,41 +5,50 @@ async function getData() {
     axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${post.id}`)
   );
 }
-
+let nextCount = 0;
+let prevCount = 0;
 async function* generateData() {
   const data = await getData();
-  for await (const v of data) {
-    yield await v.data;
+  let i = -1
+  while(++i < data.length){
+    const allData = await data[i]
+    yield allData.data
+    if(i === data.length - 1){
+        i = -1
+    }
   }
+//   for await (const v of data) {
+//     yield await v.data;
+//   }
 }
 
 async function* prevGenerateData() {
   const data = await getData();
   let i = data.length;
   while (i-- > 0) {
-    yield await data[i];
+    const allData = await data[i];
+    yield allData.data;
+    if (i === 0) {
+      i = data.length - 1;
+    }
   }
 }
-const prevData = prevGenerateData();
-(async () => {
-    const findData = await prevData.next()
-    console.log(findData.value.data)
-})();
 
+// getElement to id
 const nextBtn = document.getElementById("next_btn");
 const prevBtn = document.getElementById("prev_btn");
-const findData = generateData();
+const prevData = prevGenerateData();
 const dataContainer = document.getElementById("dataContainer");
+const nextData = generateData(nextCount);
 (async () => {
-  const { value: posts } = await findData.next();
+  const { value: posts } = await nextData.next();
   content(posts);
 })();
 // Next btn click event
 nextBtn.addEventListener("click", async function () {
   dataContainer.innerHTML = "";
-  const { value: posts, done } = await findData.next();
+  const { value: posts, done } = await nextData.next();
   if (done) {
-    console.log(true);
     const getPosts = await getData();
     const { data: posts } = await getPosts[getPosts.length - 1];
     content(posts);
@@ -48,12 +57,23 @@ nextBtn.addEventListener("click", async function () {
   }
 });
 // prev btn click event
-prevBtn.addEventListener("click", () => {});
+prevBtn.addEventListener("click", async () => {
+  dataContainer.innerHTML = "";
+  const { value: prevPost, done } = await prevData.next();
+  if (done) {
+    const getPosts = await getData();
+    const { data: posts } = await getPosts[0];
+    content(posts);
+  } else {
+    content(prevPost);
+  }
+});
 
 function content(posts) {
   posts?.forEach((post, index) => {
     const div = document.createElement("div");
     div.innerHTML = `
+             <h1>${post.userId}</h1>       
                   <p class="post_title"><span>${index + 1}</span>. ${
       post.title
     }</p>
